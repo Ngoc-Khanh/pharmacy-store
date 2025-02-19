26a7a743560bc5b9fb7b4f83b17f4d3009d82056
Ngoc-Khanh <dongockhanh2003@gmail.com>
2025-02-19 12:00:48 +0700

    Change password

--- src/page/main/index.ts
+++ src/page/main/index.ts
@@ -1,3 +1,4 @@
 export { default as HomePage } from "./home/page";
 export { default as ProductPage } from "./product/page";
 export { default as ProfilePage } from "./settings/profile/page";
+export { default as ChangePasswordPage } from "./settings/change-pwd/page";
