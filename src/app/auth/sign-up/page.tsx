7ae72ce338cb7cdd506a237340ac5ced73a724ef
Ngoc-Khanh <dongockhanh2003@gmail.com>
2025-03-18 15:24:52 +0700

    sign up

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,10 +1,207 @@
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { RegistrationForm, registrationSchema } from '@/data/zod-schemas';
+import { RainbowButton } from '@/components/magicui/rainbow-button';
+import { TextAnimate } from '@/components/magicui/text-animate';
+import { BriefcaseMedical, Loader2 } from 'lucide-react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from '@/components/ui/input';
 import { AuthLayout } from "@/layouts/auth";
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
+import { routes } from '@/config';
+import { useState } from 'react';
 
 export default function RegisterPage() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<RegistrationForm>({
+    resolver: zodResolver(registrationSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      phone: "",
+    }
+  })
+
+  const onSubmit = (data: RegistrationForm) => {
+    console.log(data);
+  }
+
   return (
     <AuthLayout title="Register">
-      <div>
-        Register Page's
+      <div className="flex flex-col gap-6">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col items-center gap-2">
+                <Link to={routes.home} className="flex flex-col items-center gap-2 font-medium">
+                  <div className="flex h-8 w-8 items-center justify-center rounded-md">
+                    <BriefcaseMedical className="size-6" />
+                  </div>
+                  <span className="sr-only">Pharmacity Inc.</span>
+                </Link>
+                <TextAnimate
+                  animation="blurInUp"
+                  by="character"
+                  className="text-xl font-bold"
+                >
+                  Become a member of Pharmacity Store.
+                </TextAnimate>
+                <div>
+                  Already have an account?{" "}
+                  <Link
+                    to={routes.login}
+                    className="underline underline-offset-4 hover:opacity-75"
+                  >
+                    Sign in
+                  </Link>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <FormField
+                  control={form.control}
+                  name="firstName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>First Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Enter your first name"
+                          tabIndex={1}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="lastName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Last Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Enter your last name"
+                          tabIndex={2}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Enter your username"
+                        tabIndex={3}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Enter your email"
+                        tabIndex={4}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Phone</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Enter your phone"
+                        tabIndex={5}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Enter your password"
+                        tabIndex={6}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Confirm your password"
+                        tabIndex={7}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <RainbowButton
+                type="submit"
+                className="w-full"
+                disabled={isLoading}
+                tabIndex={8}
+              >
+                {isLoading ? (
+                  <div className="flex items-center justify-center gap-2 text-white dark:text-black">
+                    <Loader2 className="animate-spin" />
+                    Please wait...
+                  </div>
+                ) : (
+                  <span className="text-white dark:text-black">Register</span>
+                )}
+              </RainbowButton>
+            </div>
+          </form>
+          <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+            By clicking continue, you agree to our{" "}
+            <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+          </div>
+        </Form>
       </div>
     </AuthLayout>
   )
